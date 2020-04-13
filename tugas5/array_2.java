145fd6ef58a0b1091a47f8bc376abee1dc20f19d
13020180014 <61999864+13020180014@users.noreply.github.com>
2020-04-13 20:01:03 +0700

    Add files via upload

---
+++ tugas5/array_2.java
@@ -0,0 +1,10 @@
+public class array_2 {
+public static void main(String args[]) {
+int angka[] = new int[3];
+angka[0] = 5;
+angka[1] = 6;
+angka[2] = 9;
+System.out.print ("Cetak isi array indeks ke-1 = "+angka[1]);
+System.out.println();
+}
+}
